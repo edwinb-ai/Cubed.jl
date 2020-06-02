44f9c34b61929e0025de280a254671cda15b6416
edwinb-ai <developeredwin@gmail.com>
2020-06-02 18:07:04 -0500

    :sparkles: :boom:
    Full CUDA implementation now available. Kernels are already programmed,
    still checking for some bugs by emulating serial code.

--- src/Cubed.jl
+++ src/Cubed.jl
@@ -3,6 +3,7 @@ module Cubed
 using Random
 using RandomNumbers.Xorshifts
 using ProgressMeter
+using CUDA
 
 struct Dynamic{V<:Real}
     τ::V
@@ -27,14 +28,17 @@ mutable struct System{V<:Real}
     ener::V
 end
 
-function energy!(pos, forces, syst, pot)
+function energy!(pos, forces, syst, pot, total_energy, virial)
     total_energy = 0.0f0
     virial = 0.0f0
     force = 0.0f0
     ener = 0.0f0
 
-    @inbounds for i = 1:syst.N-1
-        for j = (i + 1):syst.N
+    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = blockDim().x * gridDim().x
+
+    for i = 1:syst.N
+        for j = index:stride:syst.N
             xij = pos[1, i] - pos[1, j]
             yij = pos[2, i] - pos[2, j]
             zij = pos[3, i] - pos[3, j]
@@ -59,7 +63,7 @@ function energy!(pos, forces, syst, pot)
                     # * Force computation
                     force = pot.λ * (1.0f0/Δpos)^(pot.λ + 1.0f0)
                     force -= (pot.λ - 1.0f0) * (1.0f0/Δpos)^pot.λ
-                    force *= (pot.a / pot.temp)
+                    force *= pot.a / pot.temp
                 else
                     force = 0.0f0
                     ener = 0.0f0
@@ -70,21 +74,21 @@ function energy!(pos, forces, syst, pot)
 
             # * Update the forces
             forces[1, i] += (force * xij) / Δpos
-            forces[1, j] -= (force * xij) / Δpos
-
             forces[2, i] += (force * yij) / Δpos
-            forces[2, j] -= (force * yij) / Δpos
-
             forces[3, i] += (force * zij) / Δpos
+
+            forces[1, j] -= (force * xij) / Δpos
+            forces[2, j] -= (force * yij) / Δpos
             forces[3, j] -= (force * zij) / Δpos
 
             # * Compute the virial
             virial += (force * xij * xij) + (force * yij * yij) + (force * zij * zij)
-            virial /= Δpos
+            virial *= 3.0f0 / Δpos
         end
     end
 
-    return (total_energy, virial)
+    # return (total_energy, virial)
+    return nothing
 end
 
 function init!(positions::AbstractArray, syst::System)
@@ -117,12 +121,18 @@ function init!(positions::AbstractArray, syst::System)
 end
 
 function ermak!(positions, forces, syst, dynamics, rnd_matrix; pbc::Bool = true)
-    for j  = axes(positions, 2)
-        @inbounds for i = axes(positions, 1)
-            positions[i, j] += (forces[i, j] * dynamics.τ) + rnd_matrix[i, j]
-            if pbc
-                positions[i, j] -= syst.L * round(positions[i, j] / syst.L)
-            end
+    index = (blockIdx().x - 1) * blockDim().x + threadIdx().x
+    stride = blockDim().x * gridDim().x
+
+    for j = index:stride:syst.N
+        positions[1, j] += (forces[1, j] * dynamics.τ) + rnd_matrix[1, j]
+        positions[2, j] += (forces[2, j] * dynamics.τ) + rnd_matrix[2, j]
+        positions[3, j] += (forces[3, j] * dynamics.τ) + rnd_matrix[3, j]
+
+        if pbc
+            positions[1, j] -= syst.L * round(positions[1, j] / syst.L)
+            positions[2, j] -= syst.L * round(positions[2, j] / syst.L)
+            positions[3, j] -= syst.L * round(positions[3, j] / syst.L)
         end
     end
 end
@@ -136,6 +146,9 @@ function move(positions, forces, syst, dynamics, potential, cycles; filename = n
     total_ρ = 0
     samples = 0
 
+    nthreads = 1
+    gpu_blocks = syst.N / nthreads
+
     rnd_matrix = Matrix{Float32}(undef, 3, syst.N)
 
     # Equilibration steps
@@ -143,14 +156,32 @@ function move(positions, forces, syst, dynamics, potential, cycles; filename = n
         # * Create array of random numbers
         randn!(syst.rng, rnd_matrix)
         rnd_matrix .*= sqrt(2.0f0 * dynamics.τ)
+        cu_rand_matrix = CuArray(rnd_matrix)
 
         # * Attempt to move the particles around
         attempts += 1
-        ermak!(positions, forces, syst, dynamics, rnd_matrix)
+        CUDA.@sync begin
+            @cuda threads=nthreads blocks=gpu_blocks ermak!(positions,
+                forces,
+                syst,
+                dynamics,
+                cu_rand_matrix
+            )
+        end
+        # display(forces)
 
         # ! Initialize forces
-        fill!(forces, 0.0f0)
-        (syst.ener, syst.press) = energy!(positions, forces, syst, potential)
+        CUDA.fill!(forces, 0.0f0)
+        # (syst.ener, syst.press) = energy!(positions, forces, syst, potential)
+        CUDA.@sync begin
+            @cuda threads=nthreads blocks=gpu_blocks energy!(positions,
+                forces,
+                syst,
+                potential,
+                syst.ener,
+                syst.press
+            )
+        end
 
         # * Save to file
         if i % syst.N == 0
@@ -160,7 +191,7 @@ function move(positions, forces, syst, dynamics, potential, cycles; filename = n
             total_energy += syst.ener
 
             # * Update the total pressure of the system
-            total_pressure += syst.press / (syst.vol * 3.0)
+            total_pressure += syst.press / (syst.vol * 3.0f0)
 
             if !isnothing(filename)
                 open(filename, "a") do io
@@ -186,7 +217,7 @@ end
 
 function run()
     # * Simulation parameters
-    N = 256  # number of particles
+    N = 2048  # number of particles
     ρ = 0.76f0 # reduced density
     volumen = N / ρ # reduced volume
     Lcaja = ∛volumen # Cubic box length
@@ -194,29 +225,42 @@ function run()
     rc = Lcaja / 2.0f0
     P = 0.0f0
 
+    # gpu_blocks = N / 256
+    gpu_blocks = 1
+    nthreads = 1
+
     # Create a rng
     rng = Xoroshiro128Star(123456)
     # Initialize the system object
     syst = System(N, Lcaja, ρ, volumen, 1.0f0 / T, P, rc, rng, 0.0f0)
-    dynamic = Dynamic(0.00005f0)
+    dynamic = Dynamic(0.000005f0)
     λ = 50
     b = convert(Float32, λ / (λ - 1.0f0))
     a = convert(Float32, λ * b^(λ - 1.0f0))
     potential = Potential(a, b, 1.4737f0, 50)
 
     # Create the positions vector
-    positions = Matrix{Float32}(undef, 3, N)
+    positions = CuArray{Float32}(undef, 3, N)
     fill!(positions, 0.0f0)
-    forces = Matrix{Float32}(undef, 3, N)
+    forces = CuArray{Float32}(undef, 3, N)
     fill!(forces, 0.0f0)
     # Initialize the positions as grid
     init!(positions, syst)
-    (syst.ener, syst.press) = energy!(positions, forces, syst, potential)
+    # (syst.ener, syst.press) = energy!(positions, forces, syst, potential)
+    CUDA.@sync begin
+        @cuda threads=nthreads blocks=gpu_blocks energy!(positions,
+            forces,
+            syst,
+            potential,
+            syst.ener,
+            syst.press
+        )
+    end
     println("Initial energy: $(syst.ener / syst.N)")
 
     # * Main loop
     # Equilibration steps
-    move(positions, forces, syst, dynamic, potential, 200000)
+    move(positions, forces, syst, dynamic, potential, 10000)
 
     # # Sampling steps
     # move(positions, syst, 300000, dispm; volume = false, filename = "nvt_$(ρ)_$(P).dat")
